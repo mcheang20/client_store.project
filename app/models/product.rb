98d7dd197938eb0cc7c241de64939f3375c0cc61
mattcheang <cheangmatt@gmail.com>
2017-02-09 20:33:14 -0800

    add logo

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,6 @@ class Product < ActiveRecord::Base
   # search method for development env use LIKE
   # for production use ILIKE
   def self.search(search)
-     where("name ILIKE ?", "%#{search}%")
+     where("name LIKE ?", "%#{search}%")
   end
 end
