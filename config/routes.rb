5e6ed83f06154b3fa564f71226b34a5eac354d02
mattcheang <cheangmatt@gmail.com>
2017-02-15 15:11:59 -0800

    format changes and fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,24 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
   resources :carts do
     resources :orders, only: [:destroy]
     member do
       post :checkout
     end
   end
+
   resources :products do
     resources :pics, only: [:show, :create, :destroy]
     member do
       get :add_to_cart
     end
   end
+
   resources :categories, only: [:index, :new, :create,:show]
   resources :subscribers
 
   get "home/welcome"
   root 'home#welcome'
-
 end
